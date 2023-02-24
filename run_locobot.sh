89c423e83d9c0ce9ce3662f7e409afb53b06c740
jinglinjackychen <a2396660@gmail.com>
2023-02-24 15:30:01 +0800

    disable rviz

--- run_locobot.sh
+++ run_locobot.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 source ~/low_cost_ws/devel/setup.bash
-roslaunch locobot_control main.launch use_base:=true use_arm:=true use_camera:=true
+roslaunch locobot_control main.launch use_base:=true use_arm:=true use_camera:=true use_rviz:=false

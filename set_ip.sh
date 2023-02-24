03a1d598c13bef925eab70c3ce323985b8b19eef
jinglinjackychen <a2396660@gmail.com>
2023-02-24 12:50:08 +0800

    update set_ip.sh and environment.sh

--- set_ip.sh
+++ set_ip.sh
@@ -1,1 +1,17 @@
-source environment.sh 10.42.0.2 10.42.0.2
+#! /bin/bash
+
+if [ "$1" ]; then
+    echo "ROS MASRER $1"
+    export ROS_MASTER_URI=http://$1:11311
+else
+    echo "ROS MASRER 10.42.0.2"
+    export ROS_MASTER_URI=http://10.42.0.2:11311
+fi
+
+if [ "$2" ]; then
+    echo "ROS IP $2"
+    export ROS_IP=$2
+else
+    echo "ROS IP 10.42.0.2"
+    export ROS_IP=10.42.0.2
+fi

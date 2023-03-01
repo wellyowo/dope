a1f2c1166b4019fdc5c48a4738a9320988d7147b
jinglinjackychen <a2396660@gmail.com>
2023-03-01 12:58:08 +0800

    modified catkin_make_py3.sh

--- catkin_make_py3.sh
+++ catkin_make_py3.sh
@@ -12,11 +12,11 @@ touch ./ROS/catkin_ws/src/geometry2/CATKIN_IGNORE
 # python3 tf
 rm ./ROS/catkin_ws/src/geometry2/CATKIN_IGNORE
 catkin_make --pkg geometry2 -C ./ROS/catkin_ws \
-    --cmake-args \
-           -DCMAKE_BUILD_TYPE=Release \
-           -DPYTHON_EXECUTABLE=/usr/bin/python3 \
-           -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m \
-           -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
+            --cmake-args \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+            -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m \
+            -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
 
 catkin_make -C ./ROS/catkin_ws
 

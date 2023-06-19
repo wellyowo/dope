8e4021a23bb7e04dfff44f4c09bcc6968e805e35
locobot <welly.ee11@nycu.edu.tw>
2023-06-19 07:41:09 -0500

    Fix: set WFH env(low_cost_workspace) not to source ROS ws

--- set_wfh_workspace_env.sh
+++ set_wfh_workspace_env.sh
@@ -4,6 +4,7 @@
 load_pyrobot_env
 # source WFH workspace and set_rospkg_path
 source set_ip.sh
-source ROS/catkin_ws/devel/setup.bash 
+#source ROS/catkin_ws/devel/setup.bash 
+#source  ROS/catkin_ws/devel_isolated/setup.bash
 source set_rospackage_path.sh
 echo "finish setup WFH env"

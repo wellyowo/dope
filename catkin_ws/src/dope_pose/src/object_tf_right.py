673782f02fd51d923e9788bb1b0a18f5016ccf1d
wellyowo <welly.ee11@nycu.edu.tw>
2023-08-03 20:18:14 +0800

    Update: right dope pos with moving average 10

--- catkin_ws/src/dope_pose/src/object_tf_right.py
+++ catkin_ws/src/dope_pose/src/object_tf_right.py
@@ -3,6 +3,7 @@
 import rospy
 import tf2_ros
 import tf2_geometry_msgs
+import numpy as np
 from geometry_msgs.msg import PoseStamped
 
 class object_trans():
@@ -13,17 +14,50 @@ class object_trans():
         self.source_frame = source_frame
         self.buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.buffer)
+        self.pos_x_list = np.zeros(10)
+        self.pos_y_list = np.zeros(10)
+        self.pos_z_list = np.zeros(10)
+        self.ori_x_list = np.zeros(10)
+        self.ori_y_list = np.zeros(10)
+        self.ori_z_list = np.zeros(10)
+        self.ori_w_list = np.zeros(10)
+        self.i = 0
+        self.pose_result = PoseStamped()
         
     
     def pose_cb(self, msg):
         try: 
             transform = self.buffer.lookup_transform(self.target_frame, self.source_frame, rospy.Time(0))
             pose_transformed = tf2_geometry_msgs.do_transform_pose(msg, transform)
-            self.pose_pub.publish(pose_transformed)
+            self.pos_x_list[self.i] = pose_transformed.pose.position.x
+            self.pos_y_list[self.i] = pose_transformed.pose.position.y
+            self.pos_z_list[self.i] = pose_transformed.pose.position.z
+            self.ori_x_list[self.i] = pose_transformed.pose.orientation.x
+            self.ori_y_list[self.i] = pose_transformed.pose.orientation.y
+            self.ori_z_list[self.i] = pose_transformed.pose.orientation.z
+            self.ori_w_list[self.i] = pose_transformed.pose.orientation.w
+            if self.i == 9:
+                self.i = 0
+            else:
+                self.i += 1
+            self.pose_result.pose.position.x, self.pose_result.pose.position.y, self.pose_result.pose.position.z, self.pose_result.pose.orientation.x, self.pose_result.pose.orientation.y, self.pose_result.pose.orientation.z, self.pose_result.pose.orientation.w = self.cal_avg()
+            self.pose_pub.publish(self.pose_result)
             print("new pose published")
         except:
             print('something wrong with tf')
             pass
+    
+    def cal_avg(self):
+        pos_x = np.mean(self.pos_x_list)
+        pos_y = np.mean(self.pos_y_list)
+        pos_z = np.mean(self.pos_z_list)
+        ori_x = np.mean(self.ori_x_list)
+        ori_y = np.mean(self.ori_y_list)
+        ori_z = np.mean(self.ori_z_list)
+        ori_w = np.mean(self.ori_w_list)
+        return pos_x, pos_y, pos_z, ori_x, ori_y, ori_z ,ori_w
+        
+
 
         
         
